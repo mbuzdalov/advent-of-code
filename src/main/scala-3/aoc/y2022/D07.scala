73cc0763d951ba9218bf62f2d72aeab125acb51b
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-12-04 20:47:51 +0000

    Prettify y2022/d07.

--- src/main/scala-3/aoc/y2022/D07.scala
+++ src/main/scala-3/aoc/y2022/D07.scala
@@ -9,14 +9,14 @@ object D07 extends Runner:
     val children = new ArrayBuffer[Dir]()
     private var _fileSize, _totalSize: Long = 0
     def addFileSize(size: Long): Unit = _fileSize += size
-    def addChild(entry: Dir): Unit = children += entry
     def totalSize: Long = _totalSize
 
+    def computeTotalSize(): Long =
+      _totalSize = _fileSize + children.view.map(_.computeTotalSize()).sum
+      _totalSize
+
     def traverse(fun: Dir => Unit): Unit =
-      _totalSize = _fileSize
-      for child <- children do
-        child.traverse(fun)
-        _totalSize += child.totalSize
+      for child <- children do child.traverse(fun)
       fun(this)
 
   private def construct(input: Seq[String]): Dir =
@@ -27,20 +27,21 @@ object D07 extends Runner:
       case s"$$ cd .." => curr = curr.parent
       case s"$$ cd $child" => curr = curr.children.find(_.name == child).get
       case s"$$ ls" =>
-      case s"dir $child" => curr.addChild(Dir(child, curr))
+      case s"dir $child" => curr.children += Dir(child, curr)
       case s"$size $child" => curr.addFileSize(size.toLong)
     root
 
   override def part1(input: Seq[String]): String =
     val root = construct(input)
+    root.computeTotalSize()
     var sumGreater = 0L
     root.traverse(d => if d.totalSize <= 100000 then sumGreater += d.totalSize)
     sumGreater.toString
+
   override def part2(input: Seq[String]): String =
     val root = construct(input)
-    root.traverse(v => ())
-    val requiredMinimumSize = 30000000 - (70000000 - root.totalSize)
-    var answer = 700000000L
+    val requiredMinimumSize = 30000000 - (70000000 - root.computeTotalSize())
+    var answer = Long.MaxValue
     root.traverse(d => if d.totalSize >= requiredMinimumSize then answer = math.min(answer, d.totalSize))
     answer.toString
 end D07
