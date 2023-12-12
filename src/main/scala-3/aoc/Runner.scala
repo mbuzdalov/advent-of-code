e9223b087af9d6ba94e434d82300c2b6e4a96825
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-12-12 02:55:13 +0000

    Remove unnecessary empty line.

--- src/main/scala-3/aoc/Runner.scala
+++ src/main/scala-3/aoc/Runner.scala
@@ -4,7 +4,6 @@ abstract class Runner:
   def part1(input: IndexedSeq[String]): String
   def part2(input: IndexedSeq[String]): String
 
-
   def main(args: Array[String]): Unit =
     val input = TestReader.read(getClass)
 
