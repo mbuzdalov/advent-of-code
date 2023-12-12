8531618d59663a7bf8dd4d12be28408f99c48967
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-12-12 02:01:19 +0000

    Refactor input reading and test utilities to allow adding mains as unit tests.

--- src/main/scala-3/aoc/Runner.scala
+++ src/main/scala-3/aoc/Runner.scala
@@ -1,45 +1,18 @@
 package aoc
 
-import java.io.{BufferedReader, File, FileReader}
-import java.util.StringTokenizer
-
-import scala.annotation.tailrec
-import scala.util.Using
-
 abstract class Runner:
-  def part1(input: IndexedSeq[String]): String = throw UnsupportedOperationException("Solution not yet implemented")
-  def part2(input: IndexedSeq[String]): String = throw UnsupportedOperationException("Solution not yet implemented")
+  def part1(input: IndexedSeq[String]): String
+  def part2(input: IndexedSeq[String]): String
 
-  @tailrec
-  private def readUntilNull(br: BufferedReader, dest: String => Unit): Unit =
-    val line = br.readLine()
-    if line != null then
-      dest(line)
-      readUntilNull(br, dest)
-
-  private def runWithFile(br: BufferedReader): Unit =
-    val builder = IndexedSeq.newBuilder[String]
-    readUntilNull(br, builder += _)
-    val input = builder.result()
-
-    try println(part1(input)) catch
-      case _: UnsupportedOperationException =>
-        println("Part 1 not yet implemented")
-
-    try println(part2(input)) catch
-      case _: UnsupportedOperationException =>
-        println("Part 2 not yet implemented")
 
   def main(args: Array[String]): Unit =
-    val myClass = getClass.getName
-    val tokens = StringTokenizer(myClass, ".$")
-    assert(tokens.nextToken() == "aoc")
-    val year, day = tokens.nextToken()
-    assert(year(0) == 'y')
-    val file = File(s"inputs/${year.substring(1)}/$day.in")
-    if file.exists() then
-      Using.resource(BufferedReader(FileReader(file)))(runWithFile)
-    else
-      sys.error("The input file does not exist")
-  end main
+    val input = TestReader.read(getClass)
+
+    try println(part1(input)) catch
+      case _: NotImplementedError =>
+        println("Part 1 not yet implemented")
+    
+    try println(part2(input)) catch
+      case _: NotImplementedError =>
+        println("Part 2 not yet implemented")
 end Runner
---
+++ src/main/scala-3/aoc/TestReader.scala
@@ -0,0 +1,32 @@
+package aoc
+
+import java.io._
+import java.util._
+
+import scala.annotation.tailrec
+import scala.util.Using
+
+object TestReader:
+  @tailrec
+  private def readUntilNull(br: BufferedReader, dest: String => Unit): Unit =
+    val line = br.readLine()
+    if line != null then
+      dest(line)
+      readUntilNull(br, dest)
+
+  private def readFromFile(br: BufferedReader): IndexedSeq[String] =
+    val builder = IndexedSeq.newBuilder[String]
+    readUntilNull(br, builder += _)
+    builder.result()
+
+  def read(clazz: Class[_]): IndexedSeq[String] =
+    val tokens = StringTokenizer(clazz.getName, ".$")
+    assert(tokens.nextToken() == "aoc")
+    val year, day = tokens.nextToken()
+    assert(year(0) == 'y')
+    val file = File(s"inputs/${year.substring(1)}/$day.in")
+    if file.exists() then
+      Using.resource(BufferedReader(FileReader(file)))(readFromFile)
+    else
+      sys.error("The input file does not exist")
+end TestReader
