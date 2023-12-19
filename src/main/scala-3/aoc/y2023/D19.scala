d92096983f634a7d46bccdf2c34eb33a88b21273
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-12-19 05:56:52 +0000

    Slightly simplify y2023/d19.

--- src/main/scala-3/aoc/y2023/D19.scala
+++ src/main/scala-3/aoc/y2023/D19.scala
@@ -1,72 +1,53 @@
 package aoc.y2023
 
 import scala.annotation.tailrec
+import scala.util.chaining._
 
 import algo.SeqUtil
 import aoc.Runner
 
 object D19 extends Runner:
-  private case class Part(x: Int, m: Int, a: Int, s: Int):
-    def rating: Int = x + m + a + s
+  private def parsePart(line: String): Map[String, Int] = line match
+    case s"{x=$x,m=$m,a=$a,s=$s}" => Map("x" -> x.toInt, "m" -> m.toInt, "a" -> a.toInt, "s" -> s.toInt)
 
-  private case class PartRange(xMin: Int, xMax: Int, mMin: Int, mMax: Int, aMin: Int, aMax: Int, sMin: Int, sMax: Int):
-    def size: Long = (xMax - xMin + 1L) * (mMax - mMin + 1L) * (aMax - aMin + 1L) * (sMax - sMin + 1L)
+  private def parseMap[T](input: IndexedSeq[String], parseRule: String => T) =
+    scala.collection.mutable.HashMap[String, IndexedSeq[T]]().tap: map =>
+      input.foreach:
+        case s"$name{$chain}" => map.put(name, SeqUtil.tokenMap(chain, ",", parseRule))
+  
+  private def parseRule1(line: String)(p: Map[String, Int]): Option[String] = line match
+    case s"$k>$a:$t" => if p(k) > a.toInt then Some(t) else None
+    case s"$k<$a:$t" => if p(k) < a.toInt then Some(t) else None
+    case t => Some(t)
 
-  private def parsePart(line: String): Part = line match
-    case s"{x=$x,m=$m,a=$a,s=$s}" => Part(x.toInt, m.toInt, a.toInt, s.toInt)
-
-  private def parseRule(line: String): Part => Option[String] = line match
-    case s"x>$a:$t" => p => if p.x > a.toInt then Some(t) else None
-    case s"x<$a:$t" => p => if p.x < a.toInt then Some(t) else None
-    case s"m>$a:$t" => p => if p.m > a.toInt then Some(t) else None
-    case s"m<$a:$t" => p => if p.m < a.toInt then Some(t) else None
-    case s"a>$a:$t" => p => if p.a > a.toInt then Some(t) else None
-    case s"a<$a:$t" => p => if p.a < a.toInt then Some(t) else None
-    case s"s>$a:$t" => p => if p.s > a.toInt then Some(t) else None
-    case s"s<$a:$t" => p => if p.s < a.toInt then Some(t) else None
-    case t => _ => Some(t)
-
-  private def parseRule2(line: String): PartRange => (Option[(PartRange, String)], Option[PartRange]) = line match
-    case s"x>$a:$t" =>
+  private def parseRule2(line: String)(p: Map[String, Range]): (Option[(Map[String, Range], String)], Option[Map[String, Range]]) = line match
+    case s"$k>$a:$t" =>
       val v = a.toInt
-      p => if p.xMin > v then (Some(p, t), None) else  if p.xMax <= v then (None, Some(p)) else (Some(p.copy(xMin = v + 1), t), Some(p.copy(xMax = v)))
-    case s"x<$a:$t" =>
+      val r = p(k)
+      (if r.last > v then Some(p.updated(k, v + 1 to r.last) -> t) else None,
+        if r.head <= v then Some(p.updated(k, r.head to v)) else None)
+    case s"$k<$a:$t" =>
       val v = a.toInt
-      p => if p.xMax < v then (Some(p, t), None) else if p.xMin >= v then (None, Some(p)) else (Some(p.copy(xMax = v - 1), t), Some(p.copy(xMin = v)))
-    case s"m>$a:$t" =>
-      val v = a.toInt
-      p => if p.mMin > v then (Some(p, t), None) else  if p.mMax <= v then (None, Some(p)) else (Some(p.copy(mMin = v + 1), t), Some(p.copy(mMax = v)))
-    case s"m<$a:$t" =>
-      val v = a.toInt
-      p => if p.mMax < v then (Some(p, t), None) else if p.mMin >= v then (None, Some(p)) else (Some(p.copy(mMax = v - 1), t), Some(p.copy(mMin = v)))
-    case s"a>$a:$t" =>
-      val v = a.toInt
-      p => if p.aMin > v then (Some(p, t), None) else  if p.aMax <= v then (None, Some(p)) else (Some(p.copy(aMin = v + 1), t), Some(p.copy(aMax = v)))
-    case s"a<$a:$t" =>
-      val v = a.toInt
-      p => if p.aMax < v then (Some(p, t), None) else if p.aMin >= v then (None, Some(p)) else (Some(p.copy(aMax = v - 1), t), Some(p.copy(aMin = v)))
-    case s"s>$a:$t" =>
-      val v = a.toInt
-      p => if p.sMin > v then (Some(p, t), None) else  if p.sMax <= v then (None, Some(p)) else (Some(p.copy(sMin = v + 1), t), Some(p.copy(sMax = v)))
-    case s"s<$a:$t" =>
-      val v = a.toInt
-      p => if p.sMax < v then (Some(p, t), None) else if p.sMin >= v then (None, Some(p)) else (Some(p.copy(sMax = v - 1), t), Some(p.copy(sMin = v)))
-    case t => p => (Some(p, t), None)
+      val r = p(k)
+      (if r.head < v then Some(p.updated(k, r.head until v) -> t) else None,
+        if r.last >= v then Some(p.updated(k, v to r.last)) else None)
+    case t => (Some(p -> t), None)
 
   @tailrec
-  private def decide(map: scala.collection.mutable.HashMap[String, IndexedSeq[Part => Option[String]]],
-                     part: Part, curr: String): Boolean =
-    map(curr).flatMap(r => r(part)).head match
-      case "A" => true
-      case "R" => false
-      case next => decide(map, part, next)
+  private def decide(map: scala.collection.mutable.HashMap[String, IndexedSeq[Map[String, Int] => Option[String]]],
+                     part: Map[String, Int], curr: String): Boolean = curr match
+    case "A" => true
+    case "R" => false
+    case v => decide(map, part, map(v).flatMap(r => r(part)).head)
 
-  private def decide(map: scala.collection.mutable.HashMap[String, IndexedSeq[PartRange => (Option[(PartRange, String)], Option[PartRange])]],
-                     partRange: PartRange, curr: String): Long =
-    if curr == "A" then partRange.size else if curr == "R" then 0 else
+  private def decide(map: scala.collection.mutable.HashMap[String, IndexedSeq[Map[String, Range] => (Option[(Map[String, Range], String)], Option[Map[String, Range]])]],
+                     partRange: Map[String, Range], curr: String): Long =
+    if curr == "A" then
+      partRange.values.map(r => r.last - r.head + 1L).product
+    else if curr == "R" then 0 else
       val wf = map(curr)
       var wfi = 0
-      var r: Option[PartRange] = Some(partRange)
+      var r: Option[Map[String, Range]] = Some(partRange)
       var sum = 0L
       while r.nonEmpty do
         val (m, nm) = wf(wfi)(r.get)
@@ -78,15 +59,11 @@ object D19 extends Runner:
   override def part1(input: IndexedSeq[String]): String =
     val Seq(wfS, partsS) = SeqUtil.splitBySeparator(input, _.isEmpty)
     val parts = partsS.map(parsePart)
-    val map = scala.collection.mutable.HashMap[String, IndexedSeq[Part => Option[String]]]()
-    wfS.foreach:
-      case s"$name{$chain}" => map.put(name, SeqUtil.tokenMap(chain, ",", parseRule))
-    parts.filter(p => decide(map, p, "in")).map(_.rating).sum.toString
+    val map = parseMap(wfS, parseRule1)
+    parts.filter(p => decide(map, p, "in")).map(_.values.sum).sum.toString
 
   override def part2(input: IndexedSeq[String]): String =
     val Seq(wfS, partsS) = SeqUtil.splitBySeparator(input, _.isEmpty)
-    val map = scala.collection.mutable.HashMap[String, IndexedSeq[PartRange => (Option[(PartRange, String)], Option[PartRange])]]()
-    wfS.foreach:
-      case s"$name{$chain}" => map.put(name, SeqUtil.tokenMap(chain, ",", parseRule2))
-    decide(map, PartRange(1, 4000, 1, 4000, 1, 4000, 1, 4000), "in").toString
+    val map = parseMap(wfS, parseRule2)
+    decide(map, Map("x" -> (1 to 4000), "m" -> (1 to 4000), "a" -> (1 to 4000), "s" -> (1 to 4000)), "in").toString
 end D19
