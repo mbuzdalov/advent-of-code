6a8e7ef5f147f77945b676c813ed87fc69325a42
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-12-23 06:41:45 +0000

    Minor rewrite of y2023/d06.

--- src/main/scala-3/aoc/y2023/D06.scala
+++ src/main/scala-3/aoc/y2023/D06.scala
@@ -1,16 +1,15 @@
 package aoc.y2023
 
-import algo.SeqUtil
+import algo.{Loops, SeqUtil}
 import aoc.Runner
 
 object D06 extends Runner:
-  private def maximize(time: Int, distance: Long): Int = (0 to time).count(t => t.toLong * (time - t) > distance)
+  private def maximize(time: Int, distance: Long): Int = Loops.count(0, time + 1)(t => t.toLong * (time - t) > distance)
 
   override def part1(input: IndexedSeq[String]): String =
     val times = SeqUtil.tokenMap(input(0), "Time: ", _.toInt)
-    val distances = SeqUtil.tokenMap(input(1), "Distance: ", _.toInt)
-    val solutions = for i <- times.indices yield maximize(times(i), distances(i))
-    solutions.product.toString
+    val distances = SeqUtil.tokenMap(input(1), "Distance: ", _.toLong)
+    times.lazyZip(distances).map(maximize).product.toString
 
   override def part2(input: IndexedSeq[String]): String =
     val time = input(0).split(" ").tail.mkString("").toInt
