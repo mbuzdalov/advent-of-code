bff9a88e99ae1fcd661611041d3d4da03ad2a3b6
mbuzdalov <mbuzdalov@gmail.com>
2025-12-02 05:39:06 +0000

    Reimplement maxDivisor via Loops.any, add a comment on the speed.

--- src/main/scala-3/aoc/y2025/D02.scala
+++ src/main/scala-3/aoc/y2025/D02.scala
@@ -1,8 +1,19 @@
 package aoc.y2025
 
-import algo.SeqUtil
+import algo.{Loops, SeqUtil}
 import aoc.Runner
 
+/**
+ * This was the first dumb idea to just apply filter to the range. It worked for the data though.
+ *
+ * Can it be faster?
+ *
+ * At least sqrt(range) is possible, because we would only need to iterate over the non-repeated part,
+ * generate the rest and check for the boundaries. Trickier for Part 2 because it still needs to check all lengths
+ * but also needs to deduplicate divisible cases (like not counting AAAA again).
+ *
+ * It should also be possible to do in O(poly(number length)), but the code would be enormous already for Part 1.
+ */
 object D02 extends Runner:
   private def solve(input: IndexedSeq[String])(isOK: Long => Boolean): String =
     val answer = input.mkString.split(',').foldLeft(0L):
@@ -13,7 +24,7 @@ object D02 extends Runner:
   
   private def maxDivisor(div: Int)(in: Long): Boolean =
     val str = in.toString
-    (2 to math.min(div, str.length)).exists: l =>
+    Loops.any(2, 1 + math.min(div, str.length)): l =>
       val subLen = str.length / l
       str.length % l == 0 && str.dropRight(subLen) == str.drop(subLen)
   
